96272644697f522b9fbc7b75cde4751fe4710e69
yingding <yingdingwang@yahoo.de>
2022-08-03 17:28:33 +0200

    added reference to the ComponentActivity

--- Wear3KtxWatchFaceConfigExample/app/src/main/java/com/example/wear3ktxwatchfaceexample/editor/WatchFaceConfigActivity.kt
+++ Wear3KtxWatchFaceConfigExample/app/src/main/java/com/example/wear3ktxwatchfaceexample/editor/WatchFaceConfigActivity.kt
@@ -6,6 +6,9 @@ import androidx.activity.ComponentActivity
  * Allows user to edit certain parts of the watch face (color style, ticks displayed, minute arm
  * length) by using the [WatchFaceConfigStateHolder]. (All widgets are disabled until data is
  * loaded.)
+ *
+ * [ComponentActivity] Base class for activities that enables composition of higher level components.
+ * Reference: https://developer.android.com/reference/androidx/activity/package-summary
  */
 class WatchFaceConfigActivity : ComponentActivity() {
 }
