58c60ea5e7f2b2b25045f39e7091fee79c3e1d5d
yingding <yingdingwang@yahoo.de>
2022-08-03 22:58:31 +0200

    Added Wear Compose Activity as WatchFaceConfigActivity instead of the View System

--- Wear3KtxWatchFaceConfigExample/app/src/main/java/com/example/watchfaceconfigexample/editor/WatchFaceConfigActivity.kt
+++ Wear3KtxWatchFaceConfigExample/app/src/main/java/com/example/watchfaceconfigexample/editor/WatchFaceConfigActivity.kt
@@ -1,6 +1,20 @@
 package com.example.watchfaceconfigexample.editor
 
+import android.content.res.Configuration
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import androidx.wear.compose.material.Text
+import com.example.watchfaceconfigexample.theme.WearAppTheme
 
 /**
  * Allows user to edit certain parts of the watch face (color style, ticks displayed, minute arm
@@ -11,4 +25,45 @@ import androidx.activity.ComponentActivity
  * Reference: https://developer.android.com/reference/androidx/activity/package-summary
  */
 class WatchFaceConfigActivity : ComponentActivity() {
+    private val stateHolder: WatchFaceConfigStateHolder by lazy {
+        WatchFaceConfigStateHolder(
+            lifecycleScope,
+            this@WatchFaceConfigActivity
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent{
+            WatchfaceConfigApp()
+        }
+    }
 }
+
+@Composable
+fun WatchfaceConfigApp() {
+    WearAppTheme {
+        HelloWorld()
+    }
+}
+
+@Composable
+fun HelloWorld() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Hello World!")
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_TYPE_WATCH,
+    device = Devices.WEAR_OS_SMALL_ROUND,
+    // showBackground = false
+)
+@Composable
+private fun HelloWorldPreview() {
+    WearAppTheme {
+        HelloWorld()
+    }
+}
+
