987d3fb6120b914b44e3b423ba8cb6491e4b42a5
yingding <yingdingwang@yahoo.de>
2022-09-24 19:35:25 +0200

    clean up the unused view import

--- Wear3KtxWatchFaceConfigExample/app/src/main/java/com/example/watchfaceconfigexample/editor/WatchFaceConfigActivity.kt
+++ Wear3KtxWatchFaceConfigExample/app/src/main/java/com/example/watchfaceconfigexample/editor/WatchFaceConfigActivity.kt
@@ -4,7 +4,6 @@ import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
