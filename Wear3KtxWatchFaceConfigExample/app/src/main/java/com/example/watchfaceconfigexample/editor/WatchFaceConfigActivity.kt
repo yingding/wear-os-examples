70c9d7e9000a249091228ff54c650e0985176051
yingding <yingdingwang@yahoo.de>
2023-02-05 13:14:06 +0100

    clean up code comments

--- Wear3KtxWatchFaceConfigExample/app/src/main/java/com/example/watchfaceconfigexample/editor/WatchFaceConfigActivity.kt
+++ Wear3KtxWatchFaceConfigExample/app/src/main/java/com/example/watchfaceconfigexample/editor/WatchFaceConfigActivity.kt
@@ -1,13 +1,11 @@
 package com.example.watchfaceconfigexample.editor
 
-import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,8 +16,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.Composable
@@ -30,12 +26,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import androidx.wear.compose.material.AutoCenteringParams
@@ -64,7 +57,6 @@ import com.example.watchfaceconfigexample.editor.WatchFaceConfigStateHolder.Comp
 import com.example.watchfaceconfigexample.theme.WearAppTheme
 import com.example.watchfaceconfigexample.utils.LEFT_COMPLICATION_ID
 import com.example.watchfaceconfigexample.utils.RIGHT_COMPLICATION_ID
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.Dispatchers
 
 /**
