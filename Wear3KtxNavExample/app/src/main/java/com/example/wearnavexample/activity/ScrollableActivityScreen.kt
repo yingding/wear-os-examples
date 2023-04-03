ba0eb8114d067e6829fb14f8dddc31221b514ef8
yingding <yingdingwang@yahoo.de>
2023-04-03 20:28:46 +0200

    updated to compose 1.4.0 kt 1.8.10 and horologist 0.3.10

--- Wear3KtxNavExample/app/src/main/java/com/example/wearnavexample/activity/ScrollableActivityScreen.kt
+++ Wear3KtxNavExample/app/src/main/java/com/example/wearnavexample/activity/ScrollableActivityScreen.kt
@@ -4,19 +4,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.wear.compose.material.AutoCenteringParams
-import androidx.wear.compose.material.ScalingLazyColumn
-import androidx.wear.compose.material.ScalingLazyListState
+import androidx.wear.compose.foundation.lazy.AutoCenteringParams
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.lazy.ScalingLazyListState
+import androidx.wear.compose.foundation.rememberActiveFocusRequester
 import androidx.wear.compose.material.Text
 import com.example.wearnavexample.nav.SampleChip
-import com.google.android.horologist.compose.focus.rememberActiveFocusRequester
+
 import com.google.android.horologist.compose.rotaryinput.rotaryWithFling
 
 @Composable
 fun ScrollableActivityScreen(
     modifier: Modifier = Modifier,
     scrollState: ScalingLazyListState,
-    // focusRequester: FocusRequester = remember { FocusRequester() }
 ) {
     val focusRequester = rememberActiveFocusRequester()
 
